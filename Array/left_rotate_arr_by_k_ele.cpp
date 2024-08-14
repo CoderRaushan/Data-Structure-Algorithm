2bf324e4b0108afb89719a0b3976065941445f08
CoderRaushanSingh <kumarraushan7631867534@gmail.com>
2024-08-14 19:59:25 +0530

    array

--- Array/left_rotate_arr_by_k_ele.cpp
+++ Array/left_rotate_arr_by_k_ele.cpp
@@ -4,34 +4,43 @@
 using namespace std;
 
 // Function to reverse elements from index l to index r in the array
-void rotateLeft(int l, int r, vector<int>& arr) {
+void reverseRange(int l, int r, vector<int>& arr) {
     while (l < r) {
         swap(arr[l], arr[r]);
         l++;
         r--;
     }
 }
+
 void leftRotate(vector<int>& arr, int k) 
 {
     int n = arr.size();
-    k = k % n; 
-    rotateLeft(0, n - 1 - k, arr);
-    rotateLeft(n - k , n - 1, arr);
-    rotateLeft(0, n - 1, arr);
+    k = k % n; // In case k is greater than n
+    
+    // Step 1: Reverse the first k elements
+    reverseRange(0, k - 1, arr);
+    
+    // Step 2: Reverse the remaining n-k elements
+    reverseRange(k, n - 1, arr);
+    
+    // Step 3: Reverse the entire array
+    reverseRange(0, n - 1, arr);
 }
 
-void display(vector<int>arr)
+void display(const vector<int>& arr)
 {
-  for(auto it:arr)
-  {
-    cout<<it<<" ";
-  }
+    for (auto it : arr)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
 }
+
 int main() 
 {
     vector<int> arr = {1, 2, 3, 4, 5};
-    int d = 2;
-    leftRotate(arr, d);
+    int k = 3;
+    leftRotate(arr, k);
     display(arr);
     return 0;
 }
