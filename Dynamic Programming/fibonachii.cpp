0f7ab6b437179514960eb6539da0eda738960df5
CoderRaushan <raushankumar23082004@gmail.com>
2024-08-28 23:48:55 +0530

    dp

--- Dynamic Programming/fibonachii.cpp
+++ Dynamic Programming/fibonachii.cpp
@@ -1,14 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-// using only recursion 
+// using only recursion  not optimaized till yet
 int f(int n)
 {
- if(n<=1)return n;
- return f(n-1)+f(n-2);
+    if (n <= 1)
+        return n;
+    return f(n - 1) + f(n - 2);
 }
 int main()
 {
-int n=8;
-cout<<f(n);
-return 0;
+    int n = 20;
+    cout << f(n);
+    return 0;
 }
+// TC=2^n and SC=2^n
+// now using dp in optimized way
+
+
+int f(int n, vector<int>&dp)
+{
+    if (n <= 1)
+        return n;
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=f(n - 1,dp) + f(n - 2,dp);
+}
+int main()
+{
+    int n = 20;
+    vector<int>dp(n+1,-1);
+    cout << f(n,dp);
+    return 0;
+}
+// TC=O(n) and SC=o(n)+O(n)
