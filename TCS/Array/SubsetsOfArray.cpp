e0062b0b2abb517e0181ad1b83d2adc749484427
CoderRaushan <raushankumar23082004@gmail.com>
2025-01-31 14:17:20 +0530

    tcs array update d

---
+++ TCS/Array/SubsetsOfArray.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace  std;
+void printSubarray(vector<int>&arr,vector<vector<int>>&subset,int i, vector<int> &temp)
+{
+   
+    if(arr.size()==i)
+    {
+        subset.push_back(temp);
+        return;
+    }
+   
+    // include the current element
+    temp.push_back(arr[i]);
+    printSubarray(arr,subset,i+1,temp);
+   // exclude the current element
+    temp.pop_back();
+    printSubarray(arr,subset,i+1,temp);
+
+    //trick is to push and call i+1 and pop and call i+1
+}
+int main()
+{
+    vector<int> temp;
+    vector<int> arr = {1,2,3};
+    vector<vector<int>> subset;
+    int i=0;
+    printSubarray(arr,subset,i,temp);
+    for(auto it:subset)
+    {
+
+        for(auto el:it)
+        {
+            cout<<"{"<<el<<"}";
+        }
+        cout<<endl;
+    }
+    //tc=O(2^n)
+    //sc=O(n) 
+    return 0;
+}
