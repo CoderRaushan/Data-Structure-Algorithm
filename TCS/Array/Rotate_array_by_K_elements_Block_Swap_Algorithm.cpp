8351276b2ba0375455cb107d606aea376cc2d977
CoderRaushan <raushankumar23082004@gmail.com>
2025-02-06 06:09:42 +0530

    update tcs arr

--- TCS/Array/Rotate_array_by_K_elements_Block_Swap_Algorithm.cpp
+++ TCS/Array/Rotate_array_by_K_elements_Block_Swap_Algorithm.cpp
@@ -27,10 +27,18 @@ int main()
 {
     vector<int>arr={1, 2, 3, 4, 5, 6, 7};
     int n=arr.size();
-    int k=3;
-    swapBro(0,k-1,arr);
-    swapBro(k,n-1,arr);
-    swapBro(0,n-1,arr);
+    int k=2;
+    //left rotate array 
+    // swapBro(0,k-1,arr);//tc=O(k) 1.
+    // swapBro(k,n-1,arr);//tc=O(n-k) 2.
+    // swapBro(0,n-1,arr);//tc=O(n) 3.
+    //time complexity=O(n)
+    // space com=O(1)
+
+    // right rotate array=> just opposite of the left rotation process
+    swapBro(0,n-1,arr);//tc=O(n) 3.
+    swapBro(k,n-1,arr);//tc=O(n-k) 2.
+    swapBro(0,k-1,arr);//tc=O(k) 1.
     for(auto ele:arr)
     {
         cout<<ele<<" ";
--- TCS/Array/Sort_Elements_of_an_Array_by_Frequency.cpp
+++ TCS/Array/Sort_Elements_of_an_Array_by_Frequency.cpp
@@ -1,51 +1,53 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
-#include<map>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
 using namespace std;
- vector<int> frequencySort(vector<int>& v) 
- {
-    unordered_map<int,int>m;
-    for(auto it:v)
+vector<int> frequencySort(vector<int> &v)
+{
+    vector<int> ans;           // not cou
+    unordered_map<int, int> m; // space complexity=O(n)
+    for (auto it : v)          // Time complexity=O(n)
     {
         m[it]++;
     }
-    vector<pair<int,int>>p;
-    for(auto it:m)
+    vector<pair<int, int>> p; // space complexity=O(n)
+    for (auto it : m)         // space complexity=O(n)
     {
-        p.push_back({it.second,it.first});
+        p.push_back({it.second, it.first});
     }
-    sort(p.begin(),p.end(),
-    [&]
-    (pair<int,int>a,pair<int,int>b)
+    sort(p.begin(), p.end(),
+        [&](pair<int, int> a, pair<int, int> b)
+        {
+            if (a.first == b.first)
+            {
+            return a.second > b.second; // Sort by value in descending order
+            }
+            else
+            {
+            return a.first < b.first; // Sort by frequency in ascending order
+            }
+        });          // sorting tc=O(nlogn)+for lembda function TC=
+    for (auto it : p) // Time complexity=O(n)
     {
-        if(a.first==b.first)
+        while (it.first) // Time complexity=O(k)
         {
-            return a.second > b.second;// Sort by value in descending order
-        }
-        else
-        {
-           return a.first < b.first;// Sort by frequency in ascending order
-        }
-    }
-    );
-    for(auto it:p)
-    {
-        while(it.first)
-        {
-            cout<<it.second<<" ";
+            ans.push_back(it.second);
             it.first--;
         }
     }
- }
+    return ans;
+}
+// tc=O(nlogn)
+// tcO(n)
 int main()
 {
-    vector<int>v={1,1,2,2,2,3};
-    vector<int>ans=frequencySort(v);
-    for(auto it:ans)
+    vector<int> v = {1, 1, 2, 2, 2, 3};
+    vector<int> ans = frequencySort(v);
+    for (auto it : ans)
     {
-        cout<<it<<" "<<endl;
+        cout << it << " ";
     }
     return 0;
 }
