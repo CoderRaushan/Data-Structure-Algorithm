92aefdc06787b308f38fd486564a78466216c1d6
CoderRaushan <raushankumar23082004@gmail.com>
2025-09-16 19:08:33 +0530

    dsa

--- Array/2D_vector.cpp
+++ Array/2D_vector.cpp
@@ -1,12 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n=4;
-//  vector<vector<int>>v;
-//  vector<vector<int>>v(4);
-// vector<vector<int>>v(4,vector<int>(4));
-vector<vector<int>>v(4,vector<int>(4,7));
-cout<<v[0][0];
-return 0;
+    int n = 4;
+    // 1. Declaration
+    vector<vector<int>> v; // empty 2D vector
+
+    // 2. Initialization
+    vector<vector<int>> v1(4);                    // 4 rows, each empty
+    vector<vector<int>> v2(4, vector<int>(4));    // 4x4 with default 0
+    vector<vector<int>> v3(4, vector<int>(4, 7)); // 4x4 filled with 7
+
+    // 3. Adding elements (push_back)
+    vector<vector<int>> v;
+    v.push_back({1, 2, 3}); // row 0
+    v.push_back({4, 5, 6}); // row 1
+
+    // 5. Iterating
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // 6. Dynamic resize
+    vector<vector<int>> v(3, vector<int>(3, 0)); // 3x3 all zeros
+    v[0][1] = 5;                                 // change element
+    v[2].push_back(9);                           // add extra element in row 2
+
+    cout << v[0][0];
+    return 0;
 }
--- Binary Search/binaryViva.cpp
+++ Binary Search/binaryViva.cpp
@@ -1,7 +1,43 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int linear(int *a,int tar,int idx)
+// {
+//     int n=idx;
+//     for(int i=0;i<n;i++)
+//     {
+//         if(a[i]==tar)
+//         {
+//             return a[i];
+//         }
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//  int arr[]={1,5,62,4,2,3,28};
+//  cout<<linear(arr,78,7);
+// return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
+int binarysearch(int *a, int low, int high, int found)
+{
+    if(low>high)return -1;
+    int mid = (low + high) / 2;
+    if (a[mid] == found)
+        return a[mid];
+    else if (a[mid] < found)
+    {
+        binarysearch(a, mid + 1, high, found);
+    }
+    else
+    {
+        binarysearch(a, low, mid-1, found);
+    }
+}
 int main()
 {
- 
-return 0;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    cout << binarysearch(arr, 0, 7, 14);
 }
